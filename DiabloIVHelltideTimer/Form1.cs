4506d401ea70aae4aa5c25fc8e5190af353593cc
KillPhilBill <51147063+KillPhilBill@users.noreply.github.com>
2023-07-03 20:56:34 -0400

    create method to format sec var into time frame

--- DiabloIVHelltideTimer/Form1.cs
+++ DiabloIVHelltideTimer/Form1.cs
@@ -12,6 +12,9 @@ namespace DiabloIVHelltideTimer
 {
     public partial class Form1 : Form
     {
+        int helltideHour = 1;
+        int helltideOffMinute = 15;
+        int sixtySecondTimer = 00;
         bool helltideEventRunning = false;
         public Form1()
         {
@@ -22,6 +25,7 @@ namespace DiabloIVHelltideTimer
         private void currentTime_Tick(object sender, EventArgs e)
         {
             lblCurrentTimeDisplay.Text = DateTime.Now.ToLongTimeString();
+            lblCountdownDisplay.Text = "00:00:" + sixtySecondTimer.ToString("D2");
         }
 
     }
