c70490ffa816fff3e5084f701e6ef4bac63b670d
KillPhilBill <51147063+KillPhilBill@users.noreply.github.com>
2023-07-03 21:18:08 -0400

    formatted timer function and tested. Displays correctly

--- DiabloIVHelltideTimer/Form1.cs
+++ DiabloIVHelltideTimer/Form1.cs
@@ -12,9 +12,7 @@ namespace DiabloIVHelltideTimer
 {
     public partial class Form1 : Form
     {
-        int helltideHour = 1;
-        int helltideOffMinute = 15;
-        int sixtySecondTimer = 00;
+        int helltideSecondTimer = 4500;
         bool helltideEventRunning = false;
         public Form1()
         {
@@ -25,7 +23,20 @@ namespace DiabloIVHelltideTimer
         private void currentTime_Tick(object sender, EventArgs e)
         {
             lblCurrentTimeDisplay.Text = DateTime.Now.ToLongTimeString();
-            lblCountdownDisplay.Text = "00:00:" + sixtySecondTimer.ToString("D2");
+            lblCountdownDisplay.Text = timeFormat(helltideSecondTimer);
+            helltideSecondTimer--;
+            
+        }
+
+        private string timeFormat(int helltideSecVar)
+        {
+            double helltideHour = Math.Floor(helltideSecVar / 3600.0);
+            double helltideMinute = Math.Floor(helltideSecVar / 60.0) % 60;
+            double helltideSeconds = helltideSecVar % 60;
+            string helltideFormattedTime = helltideHour.ToString("00") + ":" + helltideMinute.ToString("00") + ":" + helltideSeconds.ToString("00");
+
+            return helltideFormattedTime;
+
         }
 
     }
